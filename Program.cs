ad4519fb5f28ca689b448824f86c9340bbe5d8bc
Santiago L Martinez <slm17slm17@gmail.com>
2022-08-02 14:05:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using inyeccionDeDependencias.Services;
+using inyeccionDeDependencias.Repositories;
+
+using System;
+
+namespace inyeccionDeDependencias
+{
+    class Program
+    {
+    static void Main(string[] args) {
+            //dependecies
+            var sender = new SmsService();
+            var connection = new OracleConnection(); 
+            var repository = new CustomerRepository(connection);
+
+            var customerService = new CustomerService(repository);
+            var communicationService = new CommunicationService(sender);
+           
+            var customers = customerService.GetCustomers();
+            var message = "";
+
+            foreach (var customer in customers)
+            {
+                communicationService.SendMessage(customer, message);
+            };
+    }
+
+    }
+}
